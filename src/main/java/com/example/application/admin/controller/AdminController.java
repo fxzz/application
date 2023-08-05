185004671ff5e2405d38fe37c00bec65533f7e83
fxzz <fxzzy3@gmail.com>
2023-08-05 11:03:22 +0900

    #8 sb-admin2를 이용해 간단한 어드민 페이지 작성
    간단한 어드민 페이지를 만들려고 한다 아직 틀만 잡아 뒀다

---
+++ src/main/java/com/example/application/admin/controller/AdminController.java
@@ -0,0 +1,20 @@
+package com.example.application.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin/index";
+    }
+
+    @GetMapping("/tables")
+    public String tables() {
+        return "admin/tables";
+    }
+}
