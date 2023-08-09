f640dd77bacf131462dd44f1db378f1fb253212e
fxzz <fxzzy3@gmail.com>
2023-08-09 16:14:41 +0900

    #9 테스트 코드 비활성화
    안써서 비활성화 했다

--- src/test/java/com/example/application/TestClass.java
+++ src/test/java/com/example/application/TestClass.java
@@ -11,26 +11,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
-@SpringBootTest
-public class TestClass  {
-
-    @Autowired
-    CommunityWriteMapper communityWriteMapper;
-
-    @Test
-    public void testCreateData() {
-        AtomicInteger count = new AtomicInteger(0);
-        IntStream.rangeClosed(1, 1000000)
-                .parallel()
-                .forEach(i -> {
-                    // 데이터 생성 로직
-                    // 여기에 데이터 생성 코드를 작성합니다.
-                    // 예: communityService.saveCommunity(new CommunityNewReqDto(...), accountId);
-                    communityWriteMapper.insertCommunity(CommunityDto.CommunityNewDto.builder().content("FFFFFFF").title("FFFFFFFFF").accountId(3L).build());
-                    count.incrementAndGet();
-                });
-
-        // 생성된 데이터의 개수를 출력합니다.
-        System.out.println("Created " + count.get() + " data.");
-    }
-}
+//@SpringBootTest
+//public class TestClass  {
+//
+//    @Autowired
+//    CommunityWriteMapper communityWriteMapper;
+//
+//    @Test
+//    public void testCreateData() {
+//        AtomicInteger count = new AtomicInteger(0);
+//        IntStream.rangeClosed(1, 1000000)
+//                .parallel()
+//                .forEach(i -> {
+//                    // 데이터 생성 로직
+//                    // 여기에 데이터 생성 코드를 작성합니다.
+//                    // 예: communityService.saveCommunity(new CommunityNewReqDto(...), accountId);
+//                    communityWriteMapper.insertCommunity(CommunityDto.CommunityNewDto.builder().content("FFFFFFF").title("FFFFFFFFF").accountId(3L).build());
+//                    count.incrementAndGet();
+//                });
+//
+//        // 생성된 데이터의 개수를 출력합니다.
+//        System.out.println("Created " + count.get() + " data.");
+//    }
+//}
