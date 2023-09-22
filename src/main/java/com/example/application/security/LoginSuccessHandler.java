e84ad3f75c4e4c2307ad9fcd468b4345aa0d3b1c
fxzz <fxzzy3@gmail.com>
2023-09-22 17:47:44 +0900

    #20 로그인시 이전 페이지로 리다이렉트
    SavedRequest 객체를 사용해 로그인 시 이전 페이지로 이동한다

--- src/main/java/com/example/application/security/LoginSuccessHandler.java
+++ src/main/java/com/example/application/security/LoginSuccessHandler.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
@@ -16,22 +18,29 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     private final LoginFailureWriteMapper loginFailureWriteMapper;
     private final LoginFailureReadMapper loginFailureReadMapper;
-    private static final String SUCCESS_URL = "/";
+
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-
         String username = RequestUtils.getUsernameFromRequest(request);
         if (userFailuresCount(username)) {
             userFailuresCountDelete(username);
         }
 
-        response.sendRedirect(SUCCESS_URL);
+
+        SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
+        if (savedRequest != null) {
+            getRedirectStrategy().sendRedirect(request, response, savedRequest.getRedirectUrl());
+        } else {
+            super.onAuthenticationSuccess(request, response, authentication);
+        }
     }
+
+
 
 
     private boolean userFailuresCount(String username) {
