1eca51beb302a16a67835e938b177d89abd71f6f
fxzz <fxzzy3@gmail.com>
2023-11-20 03:01:37 +0900

    #26 선착순 쿠폰 발급
    incr 명령어를 활용해 쿠폰 발급 버튼을 누르면 카운트를 +1 시키고 정해진 수량을 초과하면 쿠폰을 발급하지 않도록 했다. 이 방식은 원자적인 incr을 사용해 카운터를 증가시켜서 여러 스레드가 동시에 접근하더라도 정확한 쿠폰 발급 개수를 유지할 수 있게 해준다고 한다. 테스트 결과 맞다.

--- src/main/java/com/example/application/coupon/service/CouponServiceImpl.java
+++ src/main/java/com/example/application/coupon/service/CouponServiceImpl.java
@@ -3,20 +3,46 @@ package com.example.application.coupon.service;
 import com.example.application.coupon.mapper.CouponMapper;
 import com.example.application.security.UserAccount;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CouponServiceImpl implements CouponService {
 
-    private final CouponMapper couponMapper;
+    private static final String COUPON_COUNT_KEY = "_coupon_count";
+    private static final long MAX_COUPON_COUNT = 10;
+    
+    
+  //  private final CouponMapper couponMapper;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Override
     public void issueCouponToUser(String code, UserAccount userAccount) {
         // 쿠폰 유효성 검증
+        // 쿠폰 code DB에 유효 한지 체크 하고 남은 수량 반환 받아서 동적으로 바꾸기 위해 MAX_COUPON_COUNT 를 지우고 변경
 
-        // 레디스 sorted set으로 대기열, 락
+        // 쿠폰 카운터를 1증가하고 반환받음
+        Long couponCount = incrementCouponCount(code);
 
-        //db 업데이트
+        // 현재 발급된 쿠폰 수 체크
+        if (couponCount != null && couponCount <= MAX_COUPON_COUNT) {
+            // DB 에서 쿠폰 발급
+
+            log.info("쿠폰 발급되었습니다.");
+        } else {
+            log.info("쿠폰 없습니다.");
+           return;
+        }
+    }
+
+    private Long incrementCouponCount(String code) {
+        ValueOperations<String, String> valueOps = redisTemplate.opsForValue();
+        Long couponCount = valueOps.increment(code + COUPON_COUNT_KEY);
+        return couponCount;
     }
 }
